f972d0459e7503ba26ce1ef10deb590c132bdf81
Vanessa Valencia-Garcia <vanessa_vg@outlook.fr>
2025-01-30 15:37:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// 1)
+let nom = 'Valencia-Garcia';
+let prenom = 'Vanessa';
+
+alert(`Bonjour ${prenom} ${nom}`);
+
+// 2)
+
+const number = 64;
+const age = 26;
+const result = number - age;
+
+alert(`${result} années à travailler avant la retraite ! Courage ! En développant ce sera plus fun ! `);
